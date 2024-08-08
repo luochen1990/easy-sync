c8be0305d0e80da3340f17fdd3565bf4c74253a8
LuoChen <luochen1990@gmail.com>
2024-08-08 14:32:48 +0800

    add: test/*

---
+++ test/bad_impl_1/test_impl_1.py
@@ -0,0 +1,50 @@
+import asyncio
+from typing import Callable, Coroutine, TypeVar
+from typing_extensions import ParamSpec
+import easy_async
+
+
+X = TypeVar("X")
+Y = TypeVar("Y")
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+class Waitable(easy_async.Waitable[X, Y, R]):
+    ''' A class to represent the result of an async operation '''
+
+    def wait(self) -> R:
+        return asyncio.run(self._coroutine)
+
+
+def sync_compatible(fn: Callable[P, Coroutine[X, Y, R]]) -> Callable[P, Waitable[X, Y, R]]:
+
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> Waitable[X, Y, R]:
+        awaitable = fn(*args, **kwargs)
+        return Waitable(awaitable)
+
+    return wrapper
+
+
+def test_impl_1():
+
+    @sync_compatible
+    async def async_add(a: int, b: int) -> int:
+        await asyncio.sleep(1)
+        ''' Add two numbers asynchronously '''
+        return a + b
+
+    def do_sync():
+
+        print(async_add(1, 2).wait())
+        print(async_add(3, 4).wait())
+
+    do_sync()
+
+    async def async_main():
+        result = await async_add(1, 2)
+        print(result)
+
+        do_sync()
+
+    asyncio.run(async_main())
